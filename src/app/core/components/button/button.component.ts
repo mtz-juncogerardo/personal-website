62becf49a1f8ab1d85acb80458e2df40412aa05e
Gerardo Martínez <mtz.juncogerardo@gmail.com>
2021-08-23 01:08:30 -0500

    About profile design

--- src/app/core/components/button/button.component.ts
+++ src/app/core/components/button/button.component.ts
@@ -7,19 +7,21 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
   get buttonStyle() : string{
-    return `color_${this.color} type_${this.type} size_${this.size}`
+    return `color_${this.color} type_${this.type} size_${this.size} ${this.faIcon ? 'icon' : ''}`
   };
 
   @Input() color : 'primary' | 'secondary' | 'tertiary' | 'dark';
   @Input() type : 'outline' | 'solid';
-  @Input() size: 'lg' | 'sm' | 'block';
+  @Input() size: 'lg' | 'sm' | 'md' | 'block';
   @Input() btnText: string;
+  @Input() faIcon: string;
 
   constructor() {
     this.color = 'primary';
     this.type = 'solid';
     this.size = 'lg';
     this.btnText = 'click me!'
+    this.faIcon = ''
   }
 
   ngOnInit(): void {
